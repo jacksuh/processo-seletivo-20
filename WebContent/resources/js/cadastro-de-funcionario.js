148b7168eaa30587ebf709a6983ede07cb743a7f
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-04-24 20:35:29 -0300

    Salvar dados vue.js - tentativa. correção e ajuste do layout.

--- WebContent/resources/js/cadastro-de-funcionario.js
+++ WebContent/resources/js/cadastro-de-funcionario.js
@@ -1,6 +1,22 @@
-new Vue({
-	el: '#app',
-	data: {
-		msg: 'Hello World!',
-	}
-});
+var app = new Vue({
+	el:"#app",
+    data: {
+       return {
+            form: {
+                nome: '',
+                setor: '',
+                salario: '',
+                email: '',
+                idade: ''
+            }
+        }
+    },
+    methods:{
+        submit(){
+            axios.post('/funcionarios/rs/novo', this.form)
+                .then(function( response ){
+                }.bind(this));
+        	}
+		}
+    }
+})
